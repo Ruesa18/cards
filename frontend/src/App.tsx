12228ebe1ed0c1e05c13d6d3f7f88882c51141e5
Sandro Rüfenacht <sandro.ruefenacht0@gmail.com>
2022-11-04 15:37:54 +0100

    fix: watch for build process of frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import Courses from './components/Courses';
 import CardsWriting from './components/CardsWriting';
 
-
-
 function App() {
-  return (
-    <div className="App">
-      <Courses />
-      <CardsWriting />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Courses />
+			<CardsWriting />
+		</div>
+	);
 }
 
 export default App;
